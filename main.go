c57fa3091fd78f17936a02936d67653118adadb5
Yang Zed <asd0230zx@gmail.com>
2019-07-02 13:52:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -134,8 +134,8 @@ func httpGet(q string , id string) {
      ServerUrl := os.Getenv("ServerUrl")
 	q = url.QueryEscape(q)
 	
-	echo = string("小幫手正在休息維護中~ 請多多包涵~")
-//     resp, err := http.Get(ServerUrl+q+"&id="+id)
+// 	echo = string("小幫手正在休息維護中~ 請多多包涵~")
+    resp, err := http.Get(ServerUrl+q+"&id="+id)
     if err != nil {
         // handle error
 	    defer resp.Body.Close()
